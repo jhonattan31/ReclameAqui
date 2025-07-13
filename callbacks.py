71bc3c1efdb00910a650d67f63572474b1aef51c
Jhonattan Nascimento <jhonattan.nascimento.barbosa@gmail.com>
2025-07-13 14:15:26 -0300

    Ajustando Mapa e Filtros

--- callbacks.py
+++ callbacks.py
@@ -21,65 +21,74 @@ def register_callbacks(app, df):
         Output('dados-filtrados-store', 'data'),
         [Input('filtro-estado', 'value'),
          Input('filtro-status', 'value'),
-         Input('filtro-data', 'value'),
          Input('filtro-tamanho-texto', 'value')]
     )
-    def filtrar_e_armazenar_dados(estados, status, faixa_data, faixa_texto):
+    def filtrar_e_armazenar_dados(estados, status, faixa_texto):
         dff = df.copy()
         if estados:
             dff = dff[dff['ESTADO'].isin(estados)]
         if status:
             dff = dff[dff['STATUS'].isin(status)]
-        if faixa_data:
-            dff = dff[dff['ANO'].between(faixa_data[0], faixa_data[1])]
         if faixa_texto:
             dff = dff[dff['TAMANHO_TEXTO'].between(faixa_texto[0], faixa_texto[1])]
         return dff.to_dict('records')
 
-    @app.callback(Output('grafico-serie-temporal', 'figure'), Input('dados-filtrados-store', 'data'))
-    def atualizar_serie_temporal(dados):
+    @app.callback(Output('grafico-serie-temporal', 'figure'), [Input('dados-filtrados-store', 'data'), Input('filtro-data', 'value')])
+    def atualizar_serie_temporal(dados ,faixa_data):
         if not dados: return go.Figure().update_layout(title_text="Sem dados para exibir.", template="plotly_white")
+        
         dff = pd.DataFrame(dados)
+        if faixa_data:
+            dff = dff[dff['ANO'].between(faixa_data[0], faixa_data[1])]
         dff['DATA'] = pd.to_datetime(dff['DATA'])
         reclamacoes = dff.set_index('DATA').resample('ME').size().reset_index(name='CONTAGEM')
         fig = px.line(reclamacoes, x='DATA', y='CONTAGEM', title='Volume de Reclamações ao Longo do Tempo')
         fig.update_layout(margin=dict(l=40, r=20, t=40, b=20), height=300, template="plotly_white")
         return fig
 
-    @app.callback(Output('grafico-status', 'figure'), Input('dados-filtrados-store', 'data'))
-    def atualizar_grafico_status(dados):
+    @app.callback(Output('grafico-status', 'figure'), [Input('dados-filtrados-store', 'data'), Input('filtro-data', 'value')])
+    def atualizar_grafico_status(dados ,faixa_data):
         if not dados: return go.Figure().update_layout(title_text="Sem dados para exibir.", template="plotly_white")
         dff = pd.DataFrame(dados)
+        if faixa_data:
+            dff = dff[dff['ANO'].between(faixa_data[0], faixa_data[1])]
         counts = dff['STATUS'].value_counts().reset_index()
         fig = px.pie(counts, names='STATUS', values='count', title='Distribuição por Status', hole=.3)
         fig.update_layout(margin=dict(l=20, r=20, t=40, b=20), height=300, template="plotly_white")
         return fig
 
-    @app.callback(Output('grafico-estados', 'figure'), Input('dados-filtrados-store', 'data'))
-    def atualizar_grafico_estados(dados):
+    @app.callback(Output('grafico-estados', 'figure'), [Input('dados-filtrados-store', 'data'), Input('filtro-data', 'value')])
+    def atualizar_grafico_estados(dados ,faixa_data):
         if not dados: return go.Figure().update_layout(title_text="Sem dados para exibir.", template="plotly_white")
         dff = pd.DataFrame(dados)
+        if faixa_data:
+            dff = dff[dff['ANO'].between(faixa_data[0], faixa_data[1])]
         counts = dff['ESTADO'].value_counts().reset_index()
         fig = px.bar(counts, x='ESTADO', y='count', title='Volume de Reclamações por Estado')
         fig.update_layout(margin=dict(l=40, r=20, t=40, b=20), height=300, template="plotly_white")
         return fig
         
-    @app.callback(Output('grafico-tamanho-texto', 'figure'), Input('dados-filtrados-store', 'data'))
-    def atualizar_histograma_texto(dados):
+    @app.callback(Output('grafico-tamanho-texto', 'figure'), [Input('dados-filtrados-store', 'data'), Input('filtro-data', 'value')])
+    def atualizar_histograma_texto(dados ,faixa_data):
         if not dados: return go.Figure().update_layout(title_text="Sem dados para exibir.", template="plotly_white")
         dff = pd.DataFrame(dados)
+        if faixa_data:
+            dff = dff[dff['ANO'].between(faixa_data[0], faixa_data[1])]
         fig = px.histogram(dff, x='TAMANHO_TEXTO', nbins=50, title='Distribuição do Tamanho das Reclamações')
         fig.update_layout(margin=dict(l=40, r=20, t=40, b=20), height=300, template="plotly_white")
         return fig
     
     @app.callback(
         Output('wordcloud-img', 'src'),
-        Input('dados-filtrados-store', 'data')
+        [Input('dados-filtrados-store', 'data'),
+        Input('filtro-data', 'value')]
     )
-    def update_wordcloud(dados):
+    def update_wordcloud(dados ,faixa_data):
         if not dados:
-            return ''
+            return go.Figure().update_layout(title_text="Sem dados para exibir.", template="plotly_white")
         dff = pd.DataFrame(dados)
+        if faixa_data:
+            dff = dff[dff['ANO'].between(faixa_data[0], faixa_data[1])]
         words = ' '.join(dff['DESCRICAO_TOKENIZADA'].dropna())
         texto_limpo = re.sub(r"[\"\'.,;:!?()\-]", '', words)
         wc = WordCloud(width=800, height=400, background_color='white').generate(texto_limpo)
